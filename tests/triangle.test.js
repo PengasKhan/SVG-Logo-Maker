fc907f90cd9f574b19aab7cd6d4fba9257a88e8a
PengasKhan <cheesepuff42@gmail.com>
2023-11-14 15:52:40 -0600

    testing start
    might need to do more refactoring

---
+++ tests/triangle.test.js
@@ -0,0 +1,16 @@
+const Triangle = require("../lib/shapes");
+
+describe("Triangle", () => {
+  test("should make a blue triangle", () => {
+    const shape = new Triangle();
+    shape.shapeColor("blue");
+    shape.textColor("red");
+    shape.text("OOO");
+    expect(shape.render()).toEqual(
+      `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="50,0 250,0, 150,200" fill="blue" />
+        <text x="150" y="100" font-size="60" text-anchor="middle" fill="red">OOO</text>
+        </svg>`
+    );
+  });
+});
